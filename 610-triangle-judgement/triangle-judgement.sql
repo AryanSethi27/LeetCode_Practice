508d21ce317dcd5c00139c46cee17a12654c3442
AryanSethi27 <104319535+AryanSethi27@users.noreply.github.com>
2024-10-21 20:21:48 +0530

    Time: 239 ms (74.54%) | Memory: 0B (100.00%) - LeetSync

--- 610-triangle-judgement/triangle-judgement.sql
+++ 610-triangle-judgement/triangle-judgement.sql
@@ -1,1 +1,6 @@
-select x,y,z, if(x+y>z and y+z>x and x+z>y, 'Yes', 'No') as triangle from triangle
+select x, y, z, 
+CASE
+when (x+y>z and y+z>x and x+z>y) then "Yes"
+else "No"
+END as triangle
+from Triangle
